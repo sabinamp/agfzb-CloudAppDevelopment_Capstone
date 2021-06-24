cf2c1dcd896748c1ff22d7d83b5b82199a283a1b
Sabina Maria <p_sabinamaria@yahoo.com>
2021-06-24 17:18:19 +0200

    add models and templates with nav

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from django.conf import settings
 
 
 # Create your models here.
@@ -10,6 +11,40 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='car make')
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+
+
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
+class CarDealer(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    DEALER = 'Car Dealer'
+    DATABASE_ADMIN = 'Database Admin'
+    OCCUPATION_CHOICES = [
+        (DEALER, 'Car Dealer'),
+        (DATABASE_ADMIN, 'Database Admin')
+    ]
+    occupation = models.CharField(
+        null=False,
+        max_length=20,
+        choices=OCCUPATION_CHOICES,
+        default=DEALER
+    )
+    social_link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.user.username + "," + \
+               self.occupation
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,9 +54,27 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    MODEL_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30, default='Sedan')
+    dealer_id = models.ForeignKey(CarDealer, on_delete=models.DO_NOTHING)
+    type = models.CharField(max_length=5, choices=MODEL_TYPES, default=SEDAN)
+    year = models.DateField(default=now)
 
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+    def __str__(self):
+        return self.name + "," + \
+               self.type
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview(models.Model):
+    dealer = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
+    content = models.TextField()
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,16 +14,17 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
-
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    return render(request, 'djangoapp/aboutus.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    return render(request, 'djangoapp/contact.html', context)
+
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -45,10 +46,11 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
