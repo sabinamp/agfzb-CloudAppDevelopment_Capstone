0be6e080a2ca41c96f3b64e5e982fbf5a70eaa1e
Sabina Maria <p_sabinamaria@yahoo.com>
2021-07-08 19:31:53 +0200

    add js code for ibmcloud actions

--- functions/get-dealerships.js
+++ functions/get-dealerships.js
@@ -7,6 +7,36 @@
   * @return The output of this action, which must be a JSON object.
   *
   */
- function main(params) {
-	return { message: 'Hello World' };
+  function main(params) {
+  	return {
+        params: {
+          db: 'dealerships',
+          include_docs: true
+        }
+    };
 }
+
+function formatEntries(params){
+  if(!params.rows){
+        return Promise.reject({ error: "Something went wrong on the server"});
+    }else if(params.rows.length == 0){
+       return Promise.reject({ error: "The database is empty"});
+    }
+	return {
+        entries: params.rows.map((row) => {
+         return {
+            _id: row.doc._id,
+            _rev: row.doc._rev,
+            city: row.doc.city,
+            st: row.doc.st,
+            state: row.doc.state,
+            address: row.doc.address,
+            zip: row.doc.zip,
+            lat: row.doc.lat,
+            long: row.doc.long,
+            short_name: row.doc.short_name,
+            full_name: row.doc.full_name,
+         }
+        })
+    };
+  }
