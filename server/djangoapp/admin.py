642279399cea26bbf9000a647d2be928ef032d72
Sabina Maria <p_sabinamaria@yahoo.com>
2021-07-14 12:32:40 +0200

    update django models and admin models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
-
-# Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'year', 'type']
+    list_filter = ['car_make']
+    search_fields = ['name', 'car_make']
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+    list_filter = ['name']
+    search_fields = ['name']
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.timezone import now
 from django.conf import settings
@@ -11,7 +12,9 @@ from django.conf import settings
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+
 class CarMake(models.Model):
+    """ CarMake Class"""
     name = models.CharField(null=False, max_length=30, default='car make')
     description = models.CharField(max_length=1000)
 
@@ -40,13 +43,14 @@ class CarDealer(models.Model):
         default=DEALER
     )
     social_link = models.URLField(max_length=200)
+    city = models.CharField(null=False, max_length=100, default="New York")
 
     def __str__(self):
-        return self.user.username + "," + \
-               self.occupation
+        return "Dealer " + self.user.username + "," + \
+               self.city
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+#   Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
@@ -55,26 +59,46 @@ class CarDealer(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
+    YEAR_CHOICES = []
+    for r in range(now().year, 1970, -1):
+        YEAR_CHOICES.append((r, r))
     SEDAN = 'Sedan'
     SUV = 'SUV'
     WAGON = 'WAGON'
+    SPORTS = 'sports_car'
+    PASSAT = 'passat'
+    SALON = 'salon'
+    COUPE = 'coupe'
     MODEL_TYPES = [
         (SEDAN, 'Sedan'),
         (SUV, 'SUV'),
-        (WAGON, 'WAGON')
+        (WAGON, 'WAGON'),
+        (SPORTS, 'SPORTS CAR'),
+        (PASSAT, 'PASSAT'),
+        (SALON, 'SALON'),
+        (COUPE, 'COUPE')
     ]
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(null=False, max_length=30, default='Sedan')
-    dealer_id = models.ForeignKey(CarDealer, on_delete=models.DO_NOTHING)
-    type = models.CharField(max_length=5, choices=MODEL_TYPES, default=SEDAN)
-    year = models.DateField(default=now)
+    dealer_id = models.ForeignKey(CarDealer, on_delete=models.DO_NOTHING, null=True, blank=True)
+    year = models.IntegerField(choices=YEAR_CHOICES, default=now().year, null=False)
+    type = models.CharField(max_length=15, choices=MODEL_TYPES, default=SEDAN)
+
+    # year = models.PositiveIntegerField(validators=[MinValueValidator(1900), MaxValueValidator(now().year)],
+    #                                    default=now().year - 5,
+    #                                    help_text="Use the following format: <YYYY>")
 
     def __str__(self):
         return self.name + "," + \
-               self.type
+               self.type + "," + \
+               self.car_make.name
 
 
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+# a plain Python class `DealerReview` to hold review data
 class DealerReview(models.Model):
     dealer = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
-    content = models.TextField()
+    review = models.TextField()
+    sentiment = models.TextField(blank=True)
+
+    def __str__(self):
+        return "Review: " + self.review
