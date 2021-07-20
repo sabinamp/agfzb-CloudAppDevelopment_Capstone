1224d11c27839a881c1726d39b0c984b0ecdd434
Sabina Maria <p_sabinamaria@yahoo.com>
2021-07-20 13:32:28 +0200

    add localhost to allowed hosts

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "dealers4x.eu-gb.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "dealers4x.eu-gb.cf.appdomain.cloud"]
 
 # Application definition
 
@@ -117,5 +117,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
-
-
